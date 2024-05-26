1f0d307226468fd338a62848c71c72e4bf8f31c6
YoussefBoj <157843015+YoussefBoj@users.noreply.github.com>
2024-05-26 18:20:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["ICT engineering student","Frontend Developer","Youtuber"],
+    typespeed:100,
+    backspeed:100,
+    backdelay:250,
+    loop: true
+})
